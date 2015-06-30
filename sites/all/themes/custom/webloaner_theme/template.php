fc799169f2c2ef5bc8762369d5da81dfdef0d778
davit <d_magaldadze@cu.edu.ge>
2015-06-30 16:21:44 +0400

    Corrected Accrued Interest

--- sites/all/themes/custom/webloaner_theme/template.php
+++ sites/all/themes/custom/webloaner_theme/template.php
@@ -5,6 +5,9 @@
  * template.php
  */
 
-function webloaner_theme_preprocess_html(&$variables) {
-    drupal_add_library('system', 'ui.dialog');
-}
+//function webloaner_theme_preprocess_html(&$variables) {
+//    drupal_add_library('system', 'ui.dialog');
+//}
+
+
+
