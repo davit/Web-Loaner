f70ede64492ed28e8a392e01a74fc38b2bed2a81
davit <d_magaldadze@cu.edu.ge>
2015-06-30 18:43:17 +0400

    db export

--- sites/all/themes/custom/webloaner_theme/templates/field--field_accrued_interest--physical_entity_loan.tpl.php
+++ sites/all/themes/custom/webloaner_theme/templates/field--field_accrued_interest--physical_entity_loan.tpl.php
@@ -46,13 +46,13 @@
 ?>
 
 <div class="<?php print $classes; ?>"<?php print $attributes; ?>>
-  <?php if (!$label_hidden): ?>
-    <div class="field-label"<?php print $title_attributes; ?>><?php print $label ?>:&nbsp;</div>
-  <?php endif; ?>
-  <div class="field-items"<?php print $content_attributes; ?>>
-    <?php foreach ($items as $delta => $item): ?>
-      <div class="field-item <?php print $delta % 2 ? 'odd' : 'even'; ?>"<?php print $item_attributes[$delta]; ?>>
-          <?php print $node = physical_entity_form_accrued_interest(menu_get_object()->nid); ?></div>
-    <?php endforeach; ?>
-  </div>
+    <?php if (!$label_hidden): ?>
+        <div class="field-label"<?php print $title_attributes; ?>><?php print $label ?>:&nbsp;</div>
+    <?php endif; ?>
+    <div class="field-items"<?php print $content_attributes; ?>>
+        <?php foreach ($items as $delta => $item): ?>
+            <div class="field-item <?php print $delta % 2 ? 'odd' : 'even'; ?>"<?php print $item_attributes[$delta]; ?>>
+                <?php print physical_entity_form_accrued_interest(menu_get_object()->nid); ?></div>
+        <?php endforeach; ?>
+    </div>
 </div>
