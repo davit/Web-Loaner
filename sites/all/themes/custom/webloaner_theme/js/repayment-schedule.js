1ed97718ca125c3542b81540afb02038870ff79b
davit <d_magaldadze@cu.edu.ge>
2015-06-30 19:35:20 +0400

    Loan Schedule completed

---
+++ sites/all/themes/custom/webloaner_theme/js/repayment-schedule.js
@@ -0,0 +1,87 @@
+jQuery(document).ready(function ($) {
+
+    var loanTable = $('.loan-calculator-table table');
+    var loanAmount = $('.field-name-field-principal-amount .field-item');
+    var interestRate = $('.field-name-field-annual-interest-rate .field-item');
+    var monthRows = $('.field-name-field-number-of-periods .field-item');
+    var gracePeriod = $('.field-name-field-grace-period .field-item');
+
+    var tbody = loanTable.find('tbody');
+    var tfoot = loanTable.find('tfoot');
+
+    if (monthRows.text().match(/^\d+$/)) {
+        if (tbody.first('tr').length) {
+            tbody.empty();
+            tfoot.empty();
+        }
+
+        var interest = parseFloat(interestRate.text() * 0.01 / 365 * loanAmount.text()).toFixed(2); // Per month
+        var totalPayment = pmt($, interestRate.text(), monthRows.text(), loanAmount.text());
+        var principal = parseFloat(totalPayment - interest).toFixed(2);
+
+        //alert(loanAmount.text() + ' ' + interestRate.text() + ' ' + monthRows.text() + ' ' + gracePeriod.text());
+
+        var totalPaymentSum = 0;
+        var interestSum = 0;
+        var principalSum = 0;
+
+        if (gracePeriod.text() > 0) {
+            principal = 0;
+            totalPayment = interest;
+        }
+
+        var balance = loanAmount.text() - principal;
+
+        if (loanAmount.text().length !== 0
+            && interestRate.text().length !== 0
+            && monthRows.text().length !== 0
+            && gracePeriod.text().length !== 0
+        ) {
+
+            for (var rc = 1; rc <= monthRows.text(); rc++) {
+
+                if (rc == monthRows.text()) {
+                    totalPayment = parseFloat(parseFloat(totalPayment) + parseFloat(balance)).toFixed(2);
+                    principal = parseFloat(principal + balance).toFixed(2);
+                    balance = 0;
+                }
+
+                totalPaymentSum += parseFloat(totalPayment);
+                interestSum += parseFloat(interest);
+                principalSum += parseFloat(principal);
+
+                tbody.append('<tr><td>' + rc + '</td><td>'
+                + totalPayment + '</td><td>'
+                + interest + '</td><td>'
+                + principal + '</td><td>'
+                + balance + '</td></tr>');
+
+                interest = parseFloat(interestRate.text() * 0.01 / 12 * balance).toFixed(2);
+
+                if (rc >= gracePeriod.text()) {
+                    totalPayment = pmt($, interestRate.text(), monthRows.text() - gracePeriod.text(), loanAmount.text());
+                    principal = parseFloat(totalPayment - interest).toFixed(2);
+                }
+
+
+                if (balance - parseInt(balance) > 0) {
+                    balance = parseFloat(balance - principal).toFixed(2);
+                } else {
+                    balance = parseFloat(balance - principal);
+                }
+
+            }
+
+            tfoot.append('<tr><th class="loan-total"></th><th>' +
+            totalPaymentSum.toFixed(2) + '</th><th>' +
+            interestSum.toFixed(2) + '</th><th>' +
+            Math.round(principalSum).toFixed(2) +
+            '</th><th></th></tr>');
+            $('.loan-total').html('Total');
+
+            setExportLinkPaths($, loanTable);
+        }
+    }
+
+});
+
---
+++ sites/all/themes/custom/webloaner_theme/templates/node--physical_entity_loan.tpl.php
@@ -0,0 +1,53 @@
+<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+
+    <?php print $user_picture; ?>
+
+    <?php print render($title_prefix); ?>
+    <?php if (!$page): ?>
+        <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
+    <?php endif; ?>
+    <?php print render($title_suffix); ?>
+
+    <?php if ($display_submitted): ?>
+        <div class="submitted">
+            <?php print $submitted; ?>
+        </div>
+    <?php endif; ?>
+
+    <div class="content"<?php print $content_attributes; ?>>
+        <?php
+        // We hide the comments and links now so that we can render them later.
+        hide($content['comments']);
+        hide($content['links']);
+        print render($content);
+        ?>
+    </div>
+
+    <?php print render($content['links']); ?>
+
+</div>
+
+<div class="col-lg-8 loan-calculator-table">
+    <table class="table table-responsive">
+        <thead>
+        <tr>
+            <th>#</th>
+            <th>Total Payment</th>
+            <th>Interest</th>
+            <th>Principal</th>
+            <th>Balance</th>
+        </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+        <tfoot></tfoot>
+
+    </table>
+    <div class="pdf-export-ajax">
+        <a href="#" target="_blank">
+            <img typeof="foaf:Image" src="http://webloaner.ge/sites/all/modules/contrib/views_pdf/images/pdf.png"
+                 alt="Physical Entity Statement Forms" title="Physical Entity Statement Forms">
+        </a>
+    </div>
+</div>
