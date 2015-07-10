2216bff3c5d27738148ef8ee20b0772980b7da5c
davit <d_magaldadze@cu.edu.ge>
2015-07-10 19:13:27 +0400

    Corrected something

--- sites/all/modules/custom/loan_calculator/js/loan-calculator.js
+++ sites/all/modules/custom/loan_calculator/js/loan-calculator.js
@@ -117,12 +117,4 @@ var setExportLinkPaths = function($, table) {
         //pdfExport.show();
     });
 
-
-    //body.on('mouseover', '.pdf-export-ajax', function(){
-    //   $(this).find('i').css('background', '#C5C5C5');
-    //});
-    //
-    //body.on('mouseleave', '.pdf-export-ajax', function(){
-    //   $(this).find('i').css('background', 'none');
-    //});
 };
